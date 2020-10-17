bf5a845f5c20920ebd6a0f9f2e076ea3bced785c
ellie <ellie.tms1371@gmail.com>
2020-10-17 23:46:33 +0100

    add footer components

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import "../App.css";
+
+const Footer = () => {
+    return(
+        <div className="todo-footer">
+
+        </div>
+    )
+}
+
+export default Footer;
